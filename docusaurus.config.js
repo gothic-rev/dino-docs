a5ad79660a790e77e582bf20d8cc6ecb58e5af55
dixon_aj <dixo0132@algonquinlive.com>
2024-08-10 17:43:30 -0400

    Updated baseURL parameter in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://gothic-rev.github.io/dino-docs/',
+  url: 'https://gothic-rev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dino-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
