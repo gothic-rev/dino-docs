2f59669936c01cfaabad8b21201576aa9d57ec20
dixon_aj <dixo0132@algonquinlive.com>
2024-08-10 17:46:11 -0400

    Updated baseURL parameter with trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://gothic-rev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dino-docs',
+  baseUrl: '/dino-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
